aa0cf4388288c576ae2495fb70d5f4c249c9c441
Matheus <kroskamatheus@gmail.com>
2022-12-01 21:01:24 -0300

    new challenges

---
+++ howManyArguments.js
@@ -0,0 +1,7 @@
+// Create a function called args_count that returns the number of arguments provided
+// Example: args_count(1, 2, 3) -> 3
+// Example: args_count(1, 2, 3, 10) -> 4
+
+function args_count() {
+    return arguments.length;
+}
